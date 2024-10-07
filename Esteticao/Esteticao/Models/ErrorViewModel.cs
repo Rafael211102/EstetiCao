07a59dfede7b37d5e9b8ef413a01eeb630081b28
Rafael211102 <rafaelrodrigo2111@outlook.com>
2024-10-07 11:14:12 -0300

    Pagina de agendamento
    Criação da página

---
+++ Esteticao/Esteticao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Esteticao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
